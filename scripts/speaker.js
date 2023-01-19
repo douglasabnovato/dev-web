103392372ced3b196511ec01e4b0d3042127aead
douglasabnovato <douglasabnovato@gmail.com>
2023-01-19 19:59:31 -0300

    criado todas as páginas com design inicial

---
+++ scripts/speaker.js
@@ -0,0 +1,41 @@
+const sectionCards = document.querySelector("section.cards");
+
+const card = document.querySelector("div.card");
+
+const fullstacks = [
+  {
+    title: "Guia Web Dev",
+    duration: "10 dias",
+    thumb: "../files/thumb_tools/tools.jpg",
+    site_url: "https://web.dev/learn/",
+    category: "Web Developer"
+  },
+  {
+    title: "Guia Web Dev",
+    duration: "10 dias",
+    thumb: "../files/thumb_tools/tools.jpg",
+    site_url: "https://web.dev/learn/",
+    category: "Web Developer"
+  },
+  {
+    title: "Guia Web Dev",
+    duration: "10 dias",
+    thumb: "../files/thumb_tools/tools.jpg",
+    site_url: "https://web.dev/learn/",
+    category: "Web Developer"
+  },
+]
+
+fullstacks.map((fullstack) => {
+  const cardClone = card.cloneNode(true);
+  cardClone.querySelector("a.destino").href = fullstack.site_url;
+  cardClone.querySelector("img").src = fullstack.thumb;
+  cardClone.querySelector(".title").innerHTML = fullstack.title;
+  cardClone.querySelector(".info > p.text--medium").innerHTML = fullstack.duration;
+  cardClone.querySelector(".info > p.price").innerHTML = fullstack.category;
+  sectionCards.appendChild(cardClone);
+});
+
+card.remove();
+
+
