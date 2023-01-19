adb40b0110971f269cb6dd95a79b5203aaaab086
douglasabnovato <douglasabnovato@gmail.com>
2023-01-19 13:18:26 -0300

    inserido os guias

--- scripts/hard-skill.js
+++ scripts/hard-skill.js
@@ -4,53 +4,144 @@ const card = document.querySelector("div.card");
 
 const fullstacks = [
   {
-    title: "Web Dev",
+    title: "Guia Web Dev",
     duration: "10 dias",
     thumb: "../public/thumb_hard-skills/web_dev.jpg",
     site_url: "https://web.dev/learn/",
     category: "Web Developer"
   },
   {
-    title: "CSS Tricks",
+    title: "Guia CSS Tricks",
     duration: "12 dias",
     thumb: "../public/thumb_hard-skills/css-tricks.jpg",
     site_url: "https://css-tricks.com/guides/",
     category: "CSS"
   },
   {
-    title: "Open props style",
+    title: "Guia Open props style",
     duration: "15 dias",
     thumb: "../public/thumb_hard-skills/open-props.jpg",
     site_url: "https://open-props.style/",
     category: "CSS"
   },
   {
-    title: "Glitch",
+    title: "Guia Glitch",
     duration: "8 dias",
     thumb: "../public/thumb_hard-skills/glitch.jpg",
     site_url: "https://glitch.com/",
     category: "Web Developer"
   },
   {
-    title: "UI Design Daily",
+    title: "Guia UI Design Daily",
     duration: "9 dias",
     thumb: "../public/thumb_hard-skills/ui-design-daily.jpg",
     site_url: "https://uidesigndaily.com/",
     category: "Design Templates"
   },
   {
-    title: "React for Designers",
-    duration: "3 dias",
+    title: "Guia React for Designers",
+    duration: "6 dias",
     thumb: "../public/thumb_hard-skills/react-for-designers.jpg",
     site_url: "https://reactfordesigners.com/",
     category: "Web Developer"
   },
   {
-    title: "Dev Resources",
-    duration: "3 dias",
+    title: "Guia Dev Resources",
+    duration: "13 dias",
     thumb: "../public/thumb_hard-skills/dev-resources.jpg",
     site_url: "https://devresourc.es/",
     category: "Fullstack Developer"
+  },
+  {
+    title: "Guia Headless UI",
+    duration: "11 dias",
+    thumb: "../public/thumb_hard-skills/headless.jpg",
+    site_url: "https://headlessui.com/",
+    category: "Frontend Developer"
+  },
+  {
+    title: "Guia Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Guia Codepip",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/codepip.jpg",
+    site_url: "https://codepip.com/",
+    category: "CSS"
+  },
+  {
+    title: "Guia Grid Malven",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/grid-malven.jpg",
+    site_url: "https://grid.malven.co/",
+    category: "CSS"
+  },
+  {
+    title: "Guia Flex Malven",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/flex-malven.jpg",
+    site_url: "https://flexbox.malven.co/",
+    category: "CSS"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
+  },
+  {
+    title: "Grid by Example",
+    duration: "14 dias",
+    thumb: "../public/thumb_hard-skills/gridbyexample.jpg",
+    site_url: "https://gridbyexample.com/patterns/",
+    category: "CSS Advanced"
   },
 ]
 
