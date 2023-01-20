aa3966c52594af6f407b60300b30e0b47243794d
douglasabnovato <douglasabnovato@gmail.com>
2023-01-20 08:37:36 -0300

    tópico tools

--- scripts/tools.js
+++ scripts/tools.js
@@ -4,25 +4,53 @@ const card = document.querySelector("div.card");
 
 const fullstacks = [
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "Discord",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/discord.jpg",
+    site_url: "https://discord.com/",
+    category: "Comunidade"
   },
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "StackOverflow",
+    duration: "2 dias",
+    thumb: "../files/thumb_tools/stackoverflow.jpg",
+    site_url: "https://stackoverflow.com/",
+    category: "Comunidade"
   },
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "Slack",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/slack.jpg",
+    site_url: "https://slack.com/intl/pt-br",
+    category: "Comunidade"
+  },
+  {
+    title: "Pomodoro Online",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/pomodoro.jpg",
+    site_url: "https://pomofocus.io/",
+    category: "Gestão de Projeto"
+  },
+  {
+    title: "Dontpad",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/dontpad.jpg",
+    site_url: "https://dontpad.com/",
+    category: "Gestão de Projeto"
+  },
+  {
+    title: "Notion",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/notion.jpg",
+    site_url: "https://www.notion.so/",
+    category: "Gestão de Projeto"
+  },
+  {
+    title: "Trello",
+    duration: "1 dia",
+    thumb: "../files/thumb_tools/trello.jpg",
+    site_url: "https://trello.com/home",
+    category: "Gestão de Projeto"
   },
 ]
 
