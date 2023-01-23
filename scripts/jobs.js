7c4c16f3a66a54700e6349a8aeb098a7373bb662
douglasabnovato <douglasabnovato@gmail.com>
2023-01-23 08:56:03 -0300

    jobs para dev

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -4,25 +4,123 @@ const card = document.querySelector("div.card");
 
 const fullstacks = [
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "Coodesh",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/coodesh.jpg",
+    site_url: "https://coodesh.com/",
+    category: "Developer"
   },
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "Geekhunter",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/geekhunter.jpg",
+    site_url: "https://www.geekhunter.com.br/vagas",
+    category: "Developer"
   },
   {
-    title: "Guia Web Dev",
-    duration: "10 dias",
-    thumb: "../files/thumb_tools/tools.jpg",
-    site_url: "https://web.dev/learn/",
-    category: "Web Developer"
+    title: "Linkedin",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/linkedin.jpg",
+    site_url: "https://www.linkedin.com/jobs/",
+    category: "Developer"
+  },
+  {
+    title: "Polywork",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/polywork.jpg",
+    site_url: "https://coodesh.com/",
+    category: "Developer"
+  },
+  {
+    title: "Programadores de Juiz de Fora",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programadoresjuizdefora.jpg",
+    site_url: "https://programador.emjuizdefora.com/oportunidades",
+    category: "Developer"
+  },
+  {
+    title: "Programathor",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "Bossabox",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/bossabox.jpg",
+    site_url: "https://app.bossabox.com/profile/jobs/active-products",
+    category: "Developer"
+  },
+  {
+    title: "Impulso",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/impulso.jpg",
+    site_url: "https://app.impulso.team/painel",
+    category: "Developer"
+  },
+  {
+    title: "trampos",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/trampos.jpg",
+    site_url: "https://trampos.co/oportunidades/?tr=desenvolvedor",
+    category: "Developer"
+  },
+  {
+    title: "Revelo",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/revelo.jpg",
+    site_url: "https://app.careers.revelo.com/#/international/positions/list",
+    category: "Developer"
+  },
+  {
+    title: "Movile",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/movile.jpg",
+    site_url: "https://www.movile.com.br/carreiras-movile",
+    category: "Developer"
+  },
+  {
+    title: "Glassdoor",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/glassdoor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "aaaaaaaaaa",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "aaaaaaaaaa",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "aaaaaaaaaa",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "aaaaaaaaaa",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
+  },
+  {
+    title: "aaaaaaaaaa",
+    duration: "3 dias",
+    thumb: "../files/thumb_jobs/programathor.jpg",
+    site_url: "https://programathor.com.br/",
+    category: "Developer"
   },
 ]
 
